aa003c6d6781837c4d955b977fb13cd61be31256
00chu <121792542+00chu@users.noreply.github.com>
2024-07-03 18:25:02 +0900

    [Bronze II] Title: 피보나치 수, Time: 120 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+    static ArrayList<Integer> arr = new ArrayList<>();
+
+    static void fibonacci(int n) { //피보나치
+        arr.add(0);
+        arr.add(1);
+
+        for (int i = 1; i < n; i++) {
+            int sum = arr.get(arr.size() - 2) + arr.get(arr.size() - 1);
+            arr.add(sum);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine()); //테스트 케이스
+
+        fibonacci(n);
+
+        if (n == 0) {
+            System.out.println("0");
+        } else if (n == 1) {
+            System.out.println("1");
+        } else {
+            System.out.println(arr.get(arr.size() - 1));
+        }
+    }
+}
