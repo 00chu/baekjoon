a0088c5a1353c3f00858e70b0ec93305c237026e
00chu <121792542+00chu@users.noreply.github.com>
2024-12-21 21:28:56 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.36 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,24 +1,29 @@
+import java.util.Arrays;
+
 class Solution {
     public String solution(String s) {
         String answer = "";
         
-       if (s.length() == 1 && Character.isLowerCase(s.charAt(0))) {
-            s = s.toUpperCase();
+       String[] temp = s.split("");
+        System.out.println(Arrays.toString(temp));
+
+        if (s.length() == 1 && Character.isLowerCase(s.charAt(0))) {
+            temp[0] = temp[0].toUpperCase();
         } else {
             for (int i = 0; i < s.length() - 1; i++) {
-                if (i == 0 && Character.isLowerCase(s.charAt(i))) {
-                    s = s.substring(0, i) + Character.toString(s.charAt(i)).toUpperCase() + s.substring(i + 1);
+                if (i == 0 && Character.isLowerCase(temp[i].charAt(0))) {
+                    temp[i] = Character.toString(temp[i].charAt(0)).toUpperCase();
                 }
                 if (s.charAt(i) == ' ' && s.charAt(i + 1) != ' ' && Character.isLowerCase(s.charAt(i + 1))) {
-                    s = s.substring(0, i + 1) + Character.toString(s.charAt(i + 1)).toUpperCase() + s.substring(i + 2);
+                    temp[i+1] = Character.toString(temp[i + 1].charAt(0)).toUpperCase();
                 }
                 if (s.charAt(i) != ' ' && s.charAt(i + 1) != ' ' && Character.isUpperCase(s.charAt(i + 1))) {
-                    s = s.substring(0, i + 1) + Character.toString(s.charAt(i + 1)).toLowerCase() + s.substring(i + 2);
+                    temp[i+1] = Character.toString(temp[i + 1].charAt(0)).toLowerCase();
                 }
             }
         }
 
-        answer = s;
+        answer = String.join("", temp);
         
         return answer;
     }
