7136b698b551e0290b9ded85acd2b8097fa58e9e
00chu <121792542+00chu@users.noreply.github.com>
2025-04-01 23:47:35 +0900

    [Gold IV] Title: 트리, Time: 396 ms, Memory: 54932 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.java
+++ 백준/Gold/4803. 트리/트리.java
@@ -1,10 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static int[] parent;
+
+    public static int findSet(int num) {
+        if (num == parent[num]) {
+            return num;
+        } else {
+            return findSet(parent[num]);
+        }
+    }
+
+    public static void union(int num1, int num2) {
+        num1 = findSet(num1);
+        num2 = findSet(num2);
+
+        if(num2 < num1) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        if (num1 == num2) {
+            parent[num1] = 0;
+        } else {
+            parent[num2] = num1;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -18,16 +46,18 @@ public class Main {
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
 
-            int result = 0;
+            int answer;
 
             if (n == 0 && m == 0) {
                 break;
             } else if (m == 0) {
-                result = n;
+                answer = n;
             } else {
-                boolean[] used = new boolean[n + 1];
+                parent = new int[n + 1];
 
-                ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+                for (int i = 1; i < n + 1; i++) {
+                    parent[i] = i;
+                }
 
                 for (int i = 0; i < m; i++) {
                     st = new StringTokenizer(br.readLine());
@@ -35,99 +65,25 @@ public class Main {
                     int num1 = Integer.parseInt(st.nextToken());
                     int num2 = Integer.parseInt(st.nextToken());
 
-                    if (i == 0) {
-                        ArrayList<Integer> temp = new ArrayList<>();
-                        temp.add(num1);
-                        temp.add(num2);
-
-                        used[num1] = true;
-                        used[num2] = true;
-
-                        arr.add(temp);
-                    } else {
-                        if (used[num1] && used[num2]) {
-                            int num1Index = 1000;
-                            int num2Index = 1000;
-
-                            for (int j = 0; j < arr.size(); j++) {
-                                ArrayList<Integer> temp = arr.get(j);
-                                if (temp.contains(num1)) {
-                                    num1Index = j;
-                                }
-                                if (temp.contains(num2)) {
-                                    num2Index = j;
-                                }
-                            }
-                            if (num1Index != 1000 && num2Index != 1000) {
-                                if (num1Index != num2Index) {
-                                    arr.get(num1Index).addAll(arr.get(num2Index));
-                                    arr.remove(num2Index);
-                                } else {
-                                    arr.remove(num1Index);
-                                }
-                            } else if (num1Index != 1000) {
-                                arr.remove(num1Index);
-                            }
-                            else if (num2Index != 1000) {
-                                arr.remove(num2Index);
-                            }
-                        } else if (!used[num1] && !used[num2]) {
-                            ArrayList<Integer> temp = new ArrayList<>();
-                            temp.add(num1);
-                            temp.add(num2);
-
-                            used[num1] = true;
-                            used[num2] = true;
-
-                            arr.add(temp);
-                        } else if (used[num1] && !used[num2]) {
-                            used[num2] = true;
-                            for (int j = 0; j < arr.size(); j++) {
-                                if (arr.get(j).contains(num1)) {
-                                    arr.get(j).add(num2);
-                                    break;
-                                }
-                            }
-                        } else if (!used[num1] && used[num2]) {
-                            used[num1] = true;
-                            for (int j = 0; j < arr.size(); j++) {
-                                if (arr.get(j).contains(num2)) {
-                                    arr.get(j).add(num1);
-                                    break;
-                                }
-                            }
-                        } else {
-                            for (int j = 0; j < arr.size(); j++) {
-                                if (arr.get(j).contains(num1)) {
-                                    arr.get(j).add(num2);
-                                    used[num2] = true;
-                                    break;
-                                }
-                                if (arr.get(j).contains(num2)) {
-                                    arr.get(j).add(num1);
-                                    used[num1] = true;
-                                    break;
-                                }
-                            }
-                        }
-                    }
+                    union(num1, num2);
                 }
 
-                for (int i = 1; i <= n; i++) {
-                    if (!used[i]) {
-                        result++;
+                Set<Integer> set = new HashSet<>();
+                for (int i = 1; i < n + 1; i++) {
+                    int temp = findSet(i);
+                    if (temp > 0) {
+                        set.add(temp);
                     }
                 }
-
-                result += arr.size();
+                answer = set.size();
             }
 
-            if (result == 0) {
+            if (answer == 0) {
                 System.out.println("Case " + count + ": No trees.");
-            } else if (result == 1) {
+            } else if (answer == 1) {
                 System.out.println("Case " + count + ": There is one tree.");
             } else {
-                System.out.println("Case " + count + ": A forest of " + result + " trees.");
+                System.out.println("Case " + count + ": A forest of " + answer + " trees.");
             }
         }
     }
