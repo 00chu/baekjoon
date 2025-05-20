88923cf07216ea1a6afd05256a56aeebfbfd63a7
00chu <121792542+00chu@users.noreply.github.com>
2025-05-20 21:05:25 +0900

    [Silver II] Title: 나무 자르기, Time: 500 ms, Memory: 119364 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] trees = new int[N];
+        int min = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            trees[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, trees[i]);
+            min = Math.min(min, trees[i]);
+        }
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            long sum = 0;
+            for (int i = 0; i < N; i++) {
+                if (mid < trees[i]) {
+                    sum += trees[i] - mid;
+                }
+            }
+
+            if (sum >= M) {
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
