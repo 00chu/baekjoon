ea1ab87acd34f18fdbcd5921133de5e7a9d7d4b6
00chu <121792542+00chu@users.noreply.github.com>
2025-05-30 02:33:42 +0900

    [Gold IV] Title: 고냥이, Time: 172 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -1,54 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.Objects;
 
 public class Main {
-    public static int p2 = 0;
-
-    public static int checkLength(HashMap<String, Integer> map, int N, String[] s, int p1) {
-        while (p2 < s.length && map.size() <= N) {
-            map.put(s[p2], map.getOrDefault(s[p2], 0) + 1);
-            p2++;
-        }
-
-        if (Objects.equals(s[p2 - 2], s[p2 - 1])) {
-            return p2 - p1;
-        }
-        return p2 - 1 - p1;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        String[] s = br.readLine().split("");
+        String s = br.readLine();
 
         int max = 0;
+        int count = 0;
         int p1 = 0;
+        int p2 = 0;
 
-        HashMap<String, Integer> map = new HashMap<>();
+        int[] alphabet = new int[26];
 
-        while (p1 < s.length - max) {
-            if (0 < p1 && Objects.equals(s[p1 - 1], s[p1])) {
-                if (map.get(s[p1]) == 1) {
-                    map.remove(s[p1]);
-                } else {
-                    map.put(s[p1], map.get(s[p1]) - 1);
+        while (p1 < s.length() - max) {
+            while (p2 < s.length() && count <= N) {
+                if (alphabet[s.charAt(p2) - 'a']++ == 0) {
+                    count++;
                 }
-                p1 = p1 + 1;
+                p2++;
             }
 
-            max = Math.max(max, checkLength(map, N, s, p1));
+            if (s.charAt(p2 - 2) == s.charAt(p2 - 1)) {
+                max = Math.max(max, p2 - p1);
+            }
+            max = Math.max(max, p2 - 1 - p1);
 
-            if (map.get(s[p1]) == 1) {
-                map.remove(s[p1]);
-            } else {
-                map.put(s[p1], map.get(s[p1]) - 1);
+            alphabet[s.charAt(p1) - 'a']--;
+            if (alphabet[s.charAt(p1) - 'a'] == 0) {
+                count--;
             }
             p1++;
+            if (0 < p1 && Objects.equals(s.charAt(p1 - 1), s.charAt(p1))) {
+                alphabet[s.charAt(p1) - 'a']--;
+                if (alphabet[s.charAt(p1) - 'a'] == 0) {
+                    count--;
+                }
+                p1++;
+            }
         }
         System.out.println(max);
     }
