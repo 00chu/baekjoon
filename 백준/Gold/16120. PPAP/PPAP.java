7c0226bf4b4b4bd887418f56e931ff60cf845a5e
00chu <121792542+00chu@users.noreply.github.com>
2025-06-15 19:15:38 +0900

    [Gold IV] Title: PPAP, Time: 704 ms, Memory: 91012 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.java
+++ 백준/Gold/16120. PPAP/PPAP.java
@@ -1,8 +1,8 @@
-import javax.swing.text.DefaultStyledDocument;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Objects;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,48 +10,34 @@ public class Main {
 
         String[] list = br.readLine().split("");
 
-        Stack<String> left = new Stack<>();
-        Stack<String> right = new Stack<>();
+        Stack<String> stack = new Stack<>();
 
         if (list.length < 4) {
             if (list.length == 1 && Objects.equals(list[0], "P")) {
                 System.out.println("PPAP");
-                return;
             } else {
                 System.out.println("NP");
-                return;
             }
+            return;
         }
         for (int i = 0; i < list.length; i++) {
-            left.push(list[i]);
+            stack.push(list[i]);
 
-            if (4 <= left.size()) {
-                int count = 0;
-
-                for (int j = 0; j < 4; j++) {
-                    String s = left.pop();
-                    right.push(s);
-                    if ((j != 1 && Objects.equals(s, "P")) || (j == 1 && Objects.equals(s, "A"))) {
-                        count++;
-                    } else {
-                        break;
-                    }
-                }
-
-                if (count == 4) {
-                    if (i != list.length - 1) {
-                        left.push("P");
-                        right.clear();
-                    }
-                } else {
-                    while (!right.isEmpty()) {
-                        left.push(right.pop());
-                    }
+            while (stack.size() > 3
+                    && Objects.equals(stack.get(stack.size() - 4), "P")
+                    && Objects.equals(stack.get(stack.size() - 3), "P")
+                    && Objects.equals(stack.get(stack.size() - 2), "A")
+                    && Objects.equals(stack.peek(), "P")) {
+                stack.pop();
+                stack.pop();
+                stack.pop();
+                if (stack.size() == 1 && i == list.length - 1) {
+                    stack.pop();
                 }
             }
         }
 
-        if (left.isEmpty()) {
+        if (stack.isEmpty()) {
             System.out.println("PPAP");
         } else {
             System.out.println("NP");
