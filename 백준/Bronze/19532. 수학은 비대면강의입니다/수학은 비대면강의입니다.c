74d92001b649f35d16cb83b4de733dd3e7c8bbdf
00chu <121792542+00chu@users.noreply.github.com>
2026-02-11 10:30:09 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int a, b, c, d, e, f, x, y;
+
+	scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+
+	x = (c * e - b * f) / (a * e - b * d);
+	y = (a * f - c * d) / (a * e - b * d);
+
+	printf("%d %d", x, y);
+
+	return 0;
+}
